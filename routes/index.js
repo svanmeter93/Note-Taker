4e662efd76b1ce70e32f00bd757664c544b7f70a
svanmeter93 <107381935+svanmeter93@users.noreply.github.com>
2022-12-22 14:57:19 -0600

    file structure change

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const API = require("./API.js");
 const path = require("path")
 
 router.get("/notes",(req,res)=> {
-    res.sendFile(path.join(__dirname,"../public/notes.html"))
+    res.sendFile(path.join(__dirname,"../notes.html"))
 })
 
 router.use("/api", API);
 
 router.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname,"../public/index.html"))
+    res.sendFile(path.join(__dirname,"../index.html"))
 })
 
 module.exports=router
